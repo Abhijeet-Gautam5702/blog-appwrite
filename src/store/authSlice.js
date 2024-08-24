e4da72d65b68009bf43150ac731f82f42038310e
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-08-24 11:31:47 +0530

    Add redux store configurations

---
+++ src/store/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false,
+  userData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state = {
+        status: true,
+        userData: action.payload.userData,
+      };
+    },
+    logout: (state, action) => {
+      state = {
+        status: false,
+        userData: null,
+      };
+    },
+  },
+});
+
+// export the individual authSlice reducer methods (called authSlice actions when extracted individually)
+export const { login, logout } = authSlice.actions;
+
+// export the reducer of authSlice and register it in store
+export const authReducer = authSlice.reducer;
