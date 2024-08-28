9607e489c462d286254e70910ee8f6a3fa66602f
Abhijeet Gautam <abhijeetgautam572@gmail.com>
2024-08-28 15:35:22 +0530

    Add self-notes to Input.jsx reagarding React-Hook-Form & forwardRef()

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,11 @@ import React, { useId, forwardRef } from "react";
 /*
     There might be cases where a component is in a separate file (like this Input component) and being used somewhere else. But the local states are defined within the component but they would be used elsewhere.
     
-    In such cases, a reference of that component is passed wherever the component is being used, and it allows us to access the state.
+    In such cases, a reference of that component is passed wherever the component is being used, and it allows us to access the state of this particular isolated component.
+
+
+    MOREOVER,
+    React-hook-form injects its own {...register("keyName",{options})} props to control the change in the input-values. So we simply take all the props into this input component as {...props} so that react-hook-form can work.
 */
 // Note: Simply write the component conventionally and wrap it inside forwardRef() and take ref as an argument in the component function
 const Input = forwardRef(function Input(
